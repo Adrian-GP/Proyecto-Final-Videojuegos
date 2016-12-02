20595bebd432ef232da2f1be7f863fe4c5b8b1be
AdrianP94 <adrianpena_94@hotmail.com>
2016-12-02 00:48:21 -0600

    UI externo

---
+++ Assets/Scripts/jugarAhora.cs
@@ -0,0 +1,31 @@
+﻿//#pragma strict
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class jugarAhora : MonoBehaviour {
+	public void ChangeScene(string scene){
+		SceneManager.LoadScene("map1");
+	}
+
+	public void showInstructions(string scene){
+		SceneManager.LoadScene("instrucciones");
+	}
+
+	public void goBackMenu(string scene){
+		SceneManager.LoadScene("inicioScene");
+	}
+
+	public void goCredits(string scene){
+		SceneManager.LoadScene("creditosScene");
+	}
+
+	public void goStory(string scene){
+		SceneManager.LoadScene("historiaScene");
+	}
+
+	public void appQuit(string scene){
+		Application.Quit ();
+	}
+}
